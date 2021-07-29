7dd0abae831bc35435aa78dc8703ae0de686b793
Patrycja Miekina <patmiekina@gmail.com>
2021-07-29 21:07:43 +0100

    set routes ver 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :dresses
+  resources :users do
+    resources :dresses, except: [:destroy]
+  end
+  resources :dresses, only: [:destroy]
   resources :bookings
   resources :dresses do
     resources :reviews, except: [:destroy]
   end
   resources :reviews, only: [:destroy]
+  get '/about', to: 'pages#about'
+  get '/my-dresses', to: 'dresses#my_dresses'
+  get '/booking-requests', to: 'bookings#/booking_requests'
 end
